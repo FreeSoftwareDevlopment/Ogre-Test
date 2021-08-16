5b4ccfbf5d3b8beb8e8b8880065faf414c9b501b
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-08-16 08:20:25 +0200

    modified:   .gitignore new file:   CMakeLists.txt new file:   Ogre Test Game/CMakeLists.txt new file:   Ogre Test Game/Ogre Test Game.cpp new file:   Ogre Test Game/Ogre Test Game.h new file:   ogre

---
+++ Ogre Test Game/Ogre Test Game.cpp
@@ -0,0 +1,12 @@
+﻿// Ogre Test Game.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Ogre Test Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
